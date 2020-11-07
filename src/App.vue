4542992874b2e00305058b1ccb2afe5c629d3c35
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-11-07 12:07:07 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="inside">
+      <Quote />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Quote from './components/Quote.vue'
+
+const timeApi = 'http://worldtimeapi.org/api/timezone'
+const locationApi = 'https://freegeoip.app/json/'
+
+axios
+  .get(timeApi)
+  .then(response => (console.log(response)))
+
+axios
+  .get(locationApi)
+  .then(response => (console.log(response.data.city)))
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quote
+  },
+  data () {
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    getQuote: function () {
+      const randomQuoteApi = 'https://programming-quotes-api.herokuapp.com/quotes/random'
+      axios
+        .get(randomQuoteApi)
+        .then((res) => {
+          this.quote = res.data
+          console.log(this.quote)
+        })
+    }
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  color: $white;
+  font-family: 'Inter', sans-serif;
+  height: 100vh;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0; left: 0;
+    width: 100%; height: 100%;
+    background: url('./assets/bg-image-daytime-mobile.jpg') no-repeat center center;
+  }
+
+  .inside {
+    position: relative;
+    background-color: rgba(0,0,0, 40%);
+    height: 100%;
+    padding: 2rem 1.625rem;
+  }
 }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p class="text">"{{quote}}"</p>
+    <h5>{{author}}</h5>
+    <button @click="getQuote()">Show</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+  
+export default {
+  name: 'Quote',
+  
+  data: () => {
+    return {
+      quote: '',
+      author: ''
+    }
+  },
+  methods: {
+    async getQuote () {
+      const randomQuoteApi = 'https://programming-quotes-api.herokuapp.com/quotes/random'
+      const res = await axios.get(randomQuoteApi)
+      this.quote = res.data.en
+      this.author = res.data.author
+    }
+  },
+  created:
+    async function getQuote () {
+      const randomQuoteApi = 'https://programming-quotes-api.herokuapp.com/quotes/random'
+      const res = await axios.get(randomQuoteApi)
+      this.quote = res.data.en
+      this.author = res.data.author
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
